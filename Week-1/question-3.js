de40d7ae5db1573ac4e317cdf9ccbf4ad03e2c12
Shukria <shukriasul5@gmail.com>
2025-11-22 16:08:55 +0430

    Update the solution description for week-1 third question.

--- Week-1/question-3.js
+++ Week-1/question-3.js
@@ -2,17 +2,13 @@
 //Date: 11/16/2025
 //Problem: Write a function that finds the largest number in an array
 
-//Solution: This problem can be solved in two ways, using sort array method and looping through the array. 
-// 1. Using the sort method I only need to pass two arguments for the sort function and subract the first argument from the second to get a descending order and then I can return the first element as the largest one. I also used spread operator here to prevent the main array from getting sroted.
-//  2. I can declare a variable before the loop and save the first array element and then loop through the array and compare current index value with the variable value and update the variable if the current index value is greater than what is saved in the variable. As the function can only have one retrun statement. I am uisng the first solution the sort method which is very clean and readable but I have also put the second the loop solution in comments.
-
+//Solution: I can declare a variable before the loop and save the first array element and then loop through the array and compare current index value with the variable value and update the variable if the current index value is greater than what is saved in the variable.
 
 export const findLargestNum = (numArray) =>{
     if(numArray.length ==0){
         return "Array is empty"
     }
 
-    
     let largestNum = numArray[0]
     for(let i = 0 ; i < numArray.length; i++){
         if(numArray[i] > largestNum){
